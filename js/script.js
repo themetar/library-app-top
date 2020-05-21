d5f039d38eed6d7cd585b6996fdde58bf2028863
Dimitar Apostolovski <d.apostolovski@gmail.com>
2020-05-21 21:55:41 +0200

    Add crude rendering

--- js/script.js
+++ js/script.js
@@ -15,9 +15,39 @@ function addBookToLibrary(book) {
   library.push(book);
 }
 
+function makeBookDiv(book) {
+  let book_div = document.createElement('div');
+  let cover = book_div.appendChild(document.createElement('div'));
+  
+  author = document.createElement('p');
+  author.innerHTML = book.author;
+
+  title = document.createElement('p');
+  title.innerHTML = book.title;
+
+  cover.appendChild(author);
+  cover.appendChild(title);
+
+  return book_div;
+}
+
+function render() {
+  let shelf = document.querySelector('#bookshelf');
+
+  for (book of library) {
+    let book_div = makeBookDiv(book);  
+
+    shelf.appendChild(book_div);
+  }
+}
+
 // initialize
 addBookToLibrary(new Book("King Barleycorn", "Jack London", 203, true));
+addBookToLibrary(new Book("The Cosmic Puppets", "Philip K. Dick", 130, true));
+addBookToLibrary(new Book("Nightfall", "Isaac Asimov & Robert Silverberg", 376, false));
+
 
 /* crude testing */
 
 console.log(library);
+render();
